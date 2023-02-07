98bc9adfd3ec20a7caa773b05f45f145ec531b57
Tim Millard <tim@rapidpaylegal.com>
2023-02-08 01:04:37 +1100

    API Key Prompt

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"embed"
+	"flag"
 	"fmt"
 	"os"
 	"text/template"
@@ -16,21 +17,49 @@ var (
 )
 
 func main() {
+	flag.Usage = func() {
+		fmt.Println("Usage:")
+		fmt.Printf("%s [OPTIONS] <matchid>\n", os.Args[0])
+		fmt.Println()
+		fmt.Println("Options:")
+		flag.PrintDefaults()
+	}
+	auth := flag.Bool("auth", false, "configure API Key")
+	flag.Parse()
 
-	if len(os.Args) < 2 {
-		fmt.Fprintf(os.Stderr, "Usage: %s <matchid>\n", os.Args[0]) // TODO with options:		fmt.Fprintf(os.Stderr, "Usage: %s [OPTIONS] <url>\n", os.Args[0])
+	// Get Auth Key
+	if *auth {
+		promptAuth()
+	}
+	if len(flag.Args()) < 1 {
+		flag.Usage()
 		os.Exit(1)
 	}
+	apiKey, err := readAuth()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading config:", err)
+		os.Exit(1)
+	}
+	if apiKey == "" {
+		promptAuth()
+	}
 
+	// Get Match ID
+	if len(os.Args) < 2 {
+		flag.Usage()
+		os.Exit(1)
+	}
 	matchID := os.Args[1]
 
-	client := grassroots.NewClient(os.Getenv("GRASSROOTS_KEY"))
+	// Grassroots API call
+	client := grassroots.NewClient(apiKey)
 	match, err := client.GetMatch(context.Background(), matchID)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Get match error: %v\n", err)
 		os.Exit(1)
 	}
 
+	// Run template
 	template, err := template.ParseFS(templates, "templates/matchresults_text.go.tmpl")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Newspaper template error: %v\n", err)
